dadb1a5801e7ec5b054aa3684e0f179c8a8e90ac
antonsold <soldatenkovanton@mail.ru>
2017-09-22 01:34:03 +0300

    implemented

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-template <typename T = int, typename Op = plus<T>, typename RevOp = minus<T> >
+template <typename T = long long, typename Op = plus<T>, typename RevOp = minus<T> >
 class FenwickTree{
 private:
     vector<T> t;
@@ -12,13 +12,14 @@ private:
     RevOp rev_func;
 public:
     explicit FenwickTree(const vector<T>&);
-    void update(int k, int d);
-    T query(int);
-    T query(int, int);
+    void update(int, T); //adds to old value
+    void modify(int, T); //replaces old value with new (given)
+    T query(int) const;
+    T query(int, int) const;
 };
 
 template <typename T, typename Op, typename RevOp>
-void FenwickTree<T, Op, RevOp>::update(int k, int d) {
+void FenwickTree<T, Op, RevOp>::update(int k, T d) {
     while(k < n){
         t[k] = func(d, t[k]);
         k = k|(k + 1);
@@ -26,32 +27,53 @@ void FenwickTree<T, Op, RevOp>::update(int k, int d) {
 }
 
 template <typename T, typename Op, typename RevOp>
-T FenwickTree<T, Op, RevOp>::query(int k) {
+void FenwickTree<T, Op, RevOp>::modify(int k, T val) {
+    T diff = rev_func(val, query(k, k));
+    update(k, diff);
+}
+
+template <typename T, typename Op, typename RevOp>
+T FenwickTree<T, Op, RevOp>::query(int k) const{
     T result = T(0);
     while(k >= 0){
         result = func(result, t[k]);
-        k = k & (k + 1) - 1;
+        k = (k & (k + 1)) - 1;
     }
     return result;
 }
 
 template <typename T, typename Op, typename RevOp>
-T FenwickTree<T, Op, RevOp>::query(int i, int k) {
-    return rev_func(query(k), query(i));
+T FenwickTree<T, Op, RevOp>::query(int i, int k) const{
+    return rev_func(query(k), query(i - 1));
 }
 
 template <typename T, typename Op, typename RevOp>
 FenwickTree<T, Op, RevOp>::FenwickTree(const vector<T> &v) {
     n = v.size();
-    Op func;
+    t.resize(n);
     for (int i = 0; i < n; ++i)
         t[i] = T(0);
     for (int i = 0; i < n; ++i)
-        update(i, v[0]);
+        update(i, v[i]);
 };
-//
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, m, p, q, k;
+    char cmd;
+    vector<long long> v;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> k;
+        v.push_back(k);
+    }
+    FenwickTree<> ft(v);
+    cin >> m;
+    for (int i = 0; i < m; ++i){
+        cin >> cmd >> p >> q;
+        if (cmd == 's')
+            cout << ft.query(p - 1, q - 1) << endl;
+        else if (cmd == 'u')
+            ft.modify(p - 1, q);
+    }
     return 0;
 }
